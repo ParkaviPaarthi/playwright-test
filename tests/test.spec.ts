6b4db493c21e5b9cf8187ed4ba31d180e564525d
Parkavi <parkavipaarthi@gmail.com>
2025-12-26 19:40:50 +0000

    palywright test added

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,10 +1,5 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('https://www.google.com/');
-   await page.getByRole('link', { name: 'Gmail' }).click();
-  await page.getByRole('button', { name: 'Agree' }).click();
-  await page.getByRole('link', { name: 'Open the Sign into Gmail page' }).click();
- await page.getByRole('textbox', { name: 'Email or phone' }).click();
-
+  // Recording...
 });
