1ba8f97bb1553263dd375a84140d14e9f0b789f3
Parkavi <parkavipaarthi@gmail.com>
2025-12-26 13:59:21 +0000

    example playwright test created

---
+++ tests/test.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.google.com/');
+  await page.getByRole('button', { name: 'Accept all' }).click();
+
+  await page.getByRole('link', { name: 'Gmail' }).click();
+  await page.getByRole('button', { name: 'Agree' }).click();
+  await page.getByRole('link', { name: 'Open the Sign into Gmail page' }).click();
+    await page.getByRole('link', { name: 'email' }).click();
+  // await page.getByRole('textbox', { name: 'Email or phone' }).click();
+
+});
